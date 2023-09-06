1bca19d8125b4017c1c5cc4111cee93d1fce0679
Rayama01 <125508519+Rayama01@users.noreply.github.com>
2023-09-06 14:30:36 -0500

    made basic math functions add, subtract, multiply, and divide then created three variables firstNum, operator, and secondNum with a temporary prompt method then made a operate function that takes the three variables and calls one of the aforementioned functions

---
+++ script.js
@@ -0,0 +1,40 @@
+let firstNum = parseInt(prompt("enter first number: "))
+let operator = prompt("enter operator: ")
+let secondNum = parseInt(prompt("enter second number: "))
+
+let add = function(firstNum, secondNum) {
+    let sum = firstNum + secondNum
+    return sum
+}
+
+let subtract = function(firstNum, secondNum) {
+    let sum = firstNum - secondNum
+    return sum
+}
+
+let multiply = function(firstNum, secondNum) {
+    let sum = firstNum * secondNum
+    return sum
+}
+
+let divide = function(firstNum, secondNum) {
+    let sum = firstNum / secondNum
+    return sum
+}
+
+let operate = function() {
+    if(operator === "+") {
+        console.log(add(firstNum, secondNum))
+    }else if(operator === "-") {
+        console.log(subtract(firstNum, secondNum))
+    } else if (operator === "*") {
+        console.log(multiply(firstNum, secondNum))
+    } else if(operator === "/") {
+        console.log(divide(firstNum, secondNum))
+    } else {
+        console.log("que")
+    }
+}
+
+
+let result = operate();
