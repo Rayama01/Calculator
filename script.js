38ecb7d722d3b9d2562c601aedc500a2fdcde18b
Rayama01 <125508519+Rayama01@users.noreply.github.com>
2023-10-17 20:36:14 -0500

    Changed html structure including class names and changed how the buttons display their values

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const output = document.querySelector(".output")
-const curr = document.querySelector(".currentCalc")
-let firstNum
+const output = document.querySelector(".screenOutput");
+const buttons = document.querySelectorAll("[data-attt]");
+let firstNum = ''
 let operator
-let secondNum
-let displayValue 
-let currValue
+let secondNum = ''
+let sum = 0
+
 let add = function(firstNum, secondNum) {
     let sum = parseInt(firstNum) + parseInt(secondNum)
     return sum
@@ -40,25 +40,22 @@ let operate = function() {
 }
 
 
-document.getElementById("1").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `1`
 
-document.getElementById("2").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `2`  
 
-document.getElementById("3").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `3`  
-
-document.getElementById("4").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `4`  
-
-document.getElementById("5").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `5`
-
-document.getElementById("6").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `6`
-
-document.getElementById("7").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `7`
-
-document.getElementById("8").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `8`
-
-document.getElementById("9").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `9`
-
-document.getElementById("0").onclick = () => displayValue = output.textContent = parseInt(output.textContent) + `0`
-
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const btnValue = event.target.getAttribute('data-attt');
+        if(operator == "+"  ||operator == "-" ||operator == "x" ||operator == "÷") {
+            secondNum += btnValue
+            
+            output.textContent = secondNum;
+            console.log(secondNum);
+        } else {
+            firstNum += btnValue;
+            output.textContent = firstNum;
+            console.log(firstNum);
+        }
+    })
+})
 
 let result = operate();
