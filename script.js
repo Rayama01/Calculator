f22951b230e50a6a6c37539d1c1b1d4e800893d2
Rayama01 <125508519+Rayama01@users.noreply.github.com>
2023-10-18 20:48:23 -0500

    Made all operation buttons work and made it possible to sting multiple operations together when you press the equal button

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 const output = document.querySelector(".screenOutput");
 const buttons = document.querySelectorAll("[data-attt]");
+const operators = document.querySelectorAll("[data-operators]");
+const addBtn = document.querySelector(".addBtn");
+const equalBtn = document.querySelector(".equalBtn");
 let firstNum = ''
 let operator
 let secondNum = ''
 let sum = 0
 
 let add = function(firstNum, secondNum) {
-    let sum = parseInt(firstNum) + parseInt(secondNum)
+    sum = parseInt(firstNum) + parseInt(secondNum)
     return sum
 }
 
 let subtract = function(firstNum, secondNum) {
-    let sum = firstNum - secondNum
+    sum = parseInt(firstNum) - parseInt(secondNum)
     return sum
 }
 
 let multiply = function(firstNum, secondNum) {
-    let sum = firstNum * secondNum
+    sum = parseInt(firstNum) * parseInt(secondNum)
     return sum
 }
 
 let divide = function(firstNum, secondNum) {
-    let sum = firstNum / secondNum
+    sum = parseInt(firstNum) / parseInt(secondNum)
     return sum
 }
 
 let operate = function() {
     if(operator === "+") {
-        console.log(add(firstNum, secondNum))
+        output.textContent = add(firstNum, secondNum);
     }else if(operator === "-") {
-        console.log(subtract(firstNum, secondNum))
-    } else if (operator === "*") {
-        console.log(multiply(firstNum, secondNum))
-    } else if(operator === "/") {
-        console.log(divide(firstNum, secondNum))
+        output.textContent = subtract(firstNum, secondNum);
+    } else if (operator === "x") {
+        output.textContent = multiply(firstNum, secondNum);
+    } else if(operator === "÷") {
+        output.textContent = divide(firstNum, secondNum);
     } else {
         console.log("que")
     }
@@ -47,15 +50,37 @@ buttons.forEach(button => {
         const btnValue = event.target.getAttribute('data-attt');
         if(operator == "+"  ||operator == "-" ||operator == "x" ||operator == "÷") {
             secondNum += btnValue
-            
             output.textContent = secondNum;
-            console.log(secondNum);
-        } else {
+        } 
+        else {
             firstNum += btnValue;
             output.textContent = firstNum;
+            
+        }
+        if(sum >= 1 & operator == "+" ||operator == "-" ||operator == "÷") {
+            firstNum = parseInt(sum);
+            console.log(firstNum);
+        } else if (sum >= 1 & operator == "x") {
+            firstNum = parseInt(sum);
             console.log(firstNum);
         }
     })
 })
+
+operators.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const opValue = event.target.getAttribute('data-operators');
+        operator = opValue;
+        output.textContent = opValue;
+    })
+})
+
+equalBtn.addEventListener("click", () => {
+    operate();
+    firstNum = '';
+    secondNum = '';
+    operator = "";
+});
+
 
 let result = operate();
